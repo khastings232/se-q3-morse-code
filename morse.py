b79a6552bd358705ca85166f83c7707ae3b3ad35
Kyle <kylehasti@gamil.com>
2020-09-03 15:11:51 -0400

    finished

--- morse.py
+++ morse.py
@@ -9,19 +9,33 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = 'Kyle Hastings, help from Mike Boring'
 
 from morse_dict import MORSE_2_ASCII
 
 
 def decode_bits(bits):
     # your code here
-    return
+    bits = bits.strip('0')
+    timing = min([len(element) for element in bits.split("1") + bits.split("0") 
+    if element])
+    bits_new = bits.replace("0000000" * timing, '   ').replace("111" * timing, "-").replace("1" * timing, ".").replace("000" * timing, " ").replace("0" * timing, "")
+    return bits_new
 
 
 def decode_morse(morse):
     # your code here
-    return
+    morse = morse.strip()
+    result_sentence = ""
+    words = str(morse).split("   ")
+    print(words)
+    for num, word in enumerate(words):
+        chars = word.split(" ")
+        for char in chars:
+            result_sentence += MORSE_2_ASCII[char]
+        if num != len(words) - 1:
+            result_sentence += " "
+    return result_sentence
 
 
 if __name__ == '__main__':
